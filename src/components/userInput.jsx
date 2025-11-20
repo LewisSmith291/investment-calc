a3e58ecc7672583e7c8a44512f3a6c07c46c5aaa
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2025-11-20 21:43:30 +0000

    Removed currency from 2 latter inputs. need to make it so regex for them doesnt limit 2 decimal place.

--- src/components/userInput.jsx
+++ src/components/userInput.jsx
@@ -7,70 +7,107 @@ export default function UserInput(props) {
   useEffect( () => {
     setCurrencySymbol(props.currency);
     changeCurrencySymbol();
-  }, [props, currencySymbol])
+  }, [props, currencySymbol]);
 
   const [inputVal, setInputVal] = useState( {
     begInvestment: currencySymbol,
     annInvestment: currencySymbol,
-    retInvestment: currencySymbol,
-    yearInvestment: currencySymbol
+    retInvestment: "",
+    yearInvestment: ""
   });
 
-  function callUserInput(inputIdentifyer, val){
+  function callUserInput(inputIdentifier, val){
     // remove currency symbol from new value
-    var newVal = val.replace(currencySymbol, "");
+    var isCurrency = inputIdentifier === 'begInvestment' || inputIdentifier === 'annInvestment';
 
-    // Use regex to check if user is entering letters
-    // Also only allow 2 decimal places
-    if (!(/^(?:[0-9]+(?:\.[0-9]{0,2})?)?$/.test(newVal))){
-      return;
+    if (isCurrency){
+      var newVal = val.replace(currencySymbol, "");
+      // Use regex to check if user is entering letters
+      // Also only allow 2 decimal places
+      if (!(/^(?:[0-9]+(?:\.[0-9]{0,2})?)?$/.test(newVal))){
+        return;
+      }
     }
-    
+    else {
+      if (!(/^(?:[0-9]+(?:\.[0-9]{0,2})?)?$/.test(val))){
+        return;
+      }
+    }
+
     setInputVal((prev) => (
       {
         // Set the other inputs to the same, while changing the current hook to the new value
       ...prev,
-      [inputIdentifyer]: currencySymbol + newVal
+      // Check to see if the box is a currency or normal number, add prefix symbol to currencies
+      [inputIdentifier]: isCurrency ? currencySymbol + newVal : val
       }
     ));
   }
 
   function changeCurrencySymbol(){
-    var beg = inputVal.begInvestment;
-    var ann = inputVal.annInvestment;
-    var ret = inputVal.retInvestment;
-    var year = inputVal.yearInvestment;
-    setInputVal((prev) => (
+    setInputVal(() => (
       {
         begInvestment: currencySymbol + inputVal.begInvestment.replace(/[¥£$€]/g, ""),
         annInvestment: currencySymbol + inputVal.annInvestment.replace(/[¥£$€]/g, ""),
-        retInvestment: currencySymbol + inputVal.retInvestment.replace(/[¥£$€]/g, ""),
-        yearInvestment: currencySymbol + inputVal.yearInvestment.replace(/[¥£$€]/g, "")
+        retInvestment: inputVal.retInvestment,
+        yearInvestment: inputVal.yearInvestment
       }
     ));
   }
 
+  function submit(){
+
+  }
+
+  function reset(){
+
+  }
+
+
+
+  function ResetButton(){
+    return (
+      <button id="reset-button" className="button" onClick={reset}>
+        Reset Fields
+      </button>
+    )
+  }
+
+  function SubmitButton(){
+    return (
+      <button id="submit-button" className="button" onClick={submit}>
+        Submit
+      </button>
+    )
+  }
+
   return (
-    <div id="input-group">
-      <div className="input-section">
-        <label>Initial Investment</label>
-        <input type="text" required 
-        onInput={(e) => callUserInput('begInvestment', e.target.value)} value={inputVal.begInvestment}/>
+    <div id="user-input">
+      <div id="input-group">
+        <div className="input-section">
+          <label>Initial Investment</label>
+          <input type="text" required className="input-label"
+          onInput={(e) => callUserInput('begInvestment', e.target.value)} value={inputVal.begInvestment}/>
+        </div>
+        <div className="input-section">
+          <label>Annual Investment</label>
+          <input type="text" required className="input-label"
+          onInput={(e) => callUserInput('annInvestment', e.target.value)} value={inputVal.annInvestment}/>
+        </div>
+        <div className="input-section">
+          <label>Expected Return (%)</label>
+          <input type="text" required placeholder='0' className="input-label"
+          onInput={(e) => callUserInput('retInvestment', e.target.value)} value={inputVal.retInvestment}/>
+        </div>
+        <div className="input-section">
+          <label>Duration of Investment (years)</label>
+          <input type="text" required min='0' placeholder='0' className="input-label"
+          onInput={(e) => callUserInput('yearInvestment', e.target.value)} value={inputVal.yearInvestment}/>
+        </div>
       </div>
-      <div className="input-section">
-        <label>Annual Investment</label>
-        <input type="text" required 
-        onInput={(e) => callUserInput('annInvestment', e.target.value)} value={inputVal.annInvestment}/>
-      </div>
-      <div className="input-section">
-        <label>Expected Return</label>
-        <input type="text" required 
-        onInput={(e) => callUserInput('retInvestment', e.target.value)} value={inputVal.retInvestment}/>
-      </div>
-      <div className="input-section">
-        <label>Yearly Investment</label>
-        <input type="text" required 
-        onInput={(e) => callUserInput('yearInvestment', e.target.value)} value={inputVal.yearInvestment}/>
+      <div id="buttons">
+        <ResetButton />
+        <SubmitButton />
       </div>
     </div>
   )
